5c657e30a65776b1623426a7837ad2be697f6568
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-02-20 23:23:35 +0100

    Initial Commit

---
+++ Aagedal Media Player/Aagedal_Media_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aagedal_Media_PlayerApp.swift
+//  Aagedal Media Player
+//
+//  Created by Truls Aagedal on 20/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Aagedal_Media_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aagedal Media Player/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aagedal Media Player
+//
+//  Created by Truls Aagedal on 20/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
